509f91fb56237a45c6b0a3654e6da9cfbc1989e0
david.ray <david.ray@barchart.com>
2013-12-11 17:49:03 -0600

    Separate the Faux servers and put them in the library implementation projects where they belong - more work on them also

--- barchart-feed-series-client/src/main/java/com/barchart/feed/series/client/services/TestSeriesProviderClient.java
+++ barchart-feed-series-client/src/main/java/com/barchart/feed/series/client/services/TestSeriesProviderClient.java
@@ -1,12 +1,9 @@
 package com.barchart.feed.series.client.services;
 
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 import rx.Observable;
 
-import com.barchart.feed.api.consumer.MarketService;
 import com.barchart.feed.api.series.TimePoint;
 import com.barchart.feed.api.series.TimeSeries;
 import com.barchart.feed.api.series.services.HistoricalResult;
@@ -14,8 +11,8 @@ import com.barchart.feed.api.series.services.Query;
 import com.barchart.feed.api.series.temporal.Period;
 import com.barchart.feed.api.series.temporal.PeriodType;
 import com.barchart.feed.client.provider.BarchartMarketProvider;
-import com.barchart.feed.series.services.BarchartSeriesProvider;
 import com.barchart.feed.series.services.BarchartHistoricalService;
+import com.barchart.feed.series.services.BarchartSeriesProvider;
 import com.barchart.feed.series.services.QueryBuilder;
 
 /**
@@ -27,30 +24,23 @@ import com.barchart.feed.series.services.QueryBuilder;
  * @author David Ray
  *
  */
-public class BarchartSeriesProviderTest {
+public class TestSeriesProviderClient {
 	private BarchartSeriesProvider provider;
-	FauxMarketService fms;
+	
 	
 	public void testInstantiate() {
 		BarchartMarketProvider marketService = new BarchartMarketProvider("dray", "dray");
-		fms = new FauxMarketService("dray", "dray");
 		BarchartHistoricalService<HistoricalResult> historicalService = new BarchartHistoricalService<HistoricalResult>("dray", "dray");
-		//provider = new BarchartSeriesProvider(fms, historicalService);
 		provider = new BarchartSeriesProvider(marketService, historicalService);
 	}
 	
-	
 	public Observable<TimeSeries<TimePoint>> testSubscribe() {
-	    String qStr = "maxrecords=5&order=desc";
-	    Query query = QueryBuilder.create().symbol("ESZ13").period(new Period(PeriodType.MINUTE, 1)).customQuery(qStr).build();
-	    fms.preSubscribe(query);
-	    
-	    query = QueryBuilder.create().symbol("ESZ13").period(new Period(PeriodType.MINUTE, 1)).start(new DateTime().minusDays(3)).build();
-		return provider.subscribe(query);
+		Query query = QueryBuilder.create().symbol("ESZ13").period(new Period(PeriodType.MINUTE, 1)).start(new DateTime().minusDays(3)).build();
+		return provider.fetch(query);
 	}
 	
 	public static void main(String[] args) {
-		BarchartSeriesProviderTest test = new BarchartSeriesProviderTest();
+		TestSeriesProviderClient test = new TestSeriesProviderClient();
 		test.testInstantiate();
 		
 		//Test that the market service daemon thread doesn't exit and our service stays up and running.
@@ -66,6 +56,7 @@ public class BarchartSeriesProviderTest {
 		
 		System.out.println("now testing subscribe");
 		Observable<TimeSeries<TimePoint>> observable = test.testSubscribe();
-		
+		if(observable != null)
+			observable.count(); //Do nothing
 	}
 }
